64f06285f7b446f19197ecb75c5adc98bd96db41
matsudayousuke <matsudayousuke@gmail.com>
2012-04-08 12:41:39 +0900

    タブを空白に変更

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -8,23 +8,23 @@ class ScheduleController < ApplicationController
   include SortHelper
 
   def index
-		retrieve_query
+    retrieve_query
     sort_init(@query.sort_criteria.empty? ? [['id', 'desc']] : @query.sort_criteria)
     sort_update(@query.sortable_columns)
-		if @query.valid?
-		  @limit = per_page_option
-		  @issue_count = @query.issue_count
-		  @issue_pages = Paginator.new self, @issue_count, @limit, params['page']
-		  @offset ||= @issue_pages.current.offset
-		  @issues = @query.issues(:include => [:assigned_to, :tracker, :priority, :category, :fixed_version],
-		                          :order => sort_clause,
-		                          :offset => @offset,
-		                          :limit => @limit)
-		  @issue_count_by_group = @query.issue_count_by_group
-		  respond_to do |format|
-		    format.html { render :template => 'schedule/index', :layout => !request.xhr? }
-		  end
-	  end
+    if @query.valid?
+      @limit = per_page_option
+      @issue_count = @query.issue_count
+      @issue_pages = Paginator.new self, @issue_count, @limit, params['page']
+      @offset ||= @issue_pages.current.offset
+      @issues = @query.issues(:include => [:assigned_to, :tracker, :priority, :category, :fixed_version],
+                              :order => sort_clause,
+                              :offset => @offset,
+                              :limit => @limit)
+      @issue_count_by_group = @query.issue_count_by_group
+      respond_to do |format|
+        format.html { render :template => 'schedule/index', :layout => !request.xhr? }
+      end
+    end
   end
 
   def find_project
